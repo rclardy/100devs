83f547420305054b7f94c355eb2feabc3b4d6beb
CodingClardy <28507502+CodingClardy@users.noreply.github.com>
2023-10-28 23:31:44 -0700

    Playing with classes

--- classes/class31-materials/objects-child-class/js/main.js
+++ classes/class31-materials/objects-child-class/js/main.js
@@ -1,47 +1,90 @@
 //Create an a class and extend it - Can be anything you would like it to be!
 
-class Food {
-  constructor(catagory) {
-    this.catagory = catagory;
-  }
-  munch() {
-    console.log(`${this.catagory} yummy food.`);
-  }
-}
-
-class Meat extends Food {
-  constructor(catagory, animal, cut) {
-    super(catagory);
-    this.animal = animal;
-    this.cut = cut;
-  }
-}
-
-let beef = new Meat('meat', 'cow', 'tri-tip');
-
 class Animal {
   constructor(name) {
-    this.name = name;
+    this._name = name;
+  }
+  get name() {
+    return this._name;
   }
   speak() {
-    console.log(`${this.name} makes a sound`);
+    console.log(`${this._name} makes a sound`);
   }
 }
 
 class Dog extends Animal {
   constructor(name, breed) {
     super(name);
-    this.breed = breed;
+    this._breed = breed;
+  }
+  get breed() {
+    return this._breed;
+  }
+  speak() {
+    super.speak();
+    console.log(`${this.name} barks`);
   }
 }
-
-let simba = new Dog('Simba', 'Shepard');
 
 class Cat extends Animal {
   constructor(name, breed) {
     super(name);
-    this.breed = breed;
+    this._breed = breed;
+  }
+  get breed() {
+    return this.breed;
+  }
+  speak() {
+    super.speak();
+    console.log(`${this.name} meows`);
   }
 }
 
-let juniper = new Cat('Juniper', 'Rag Doll');
+// class Animal {
+//   constructor(name) {
+//     this._name = name;
+//   }
+//   get name() {
+//     return this._name;
+//   }
+//   speak() {
+//     console.log(`${this.name} makes a sound`);
+//   }
+// }
+
+// let simba = new Animal('Simba');
+
+// class Dog extends Animal {
+//   constructor(name, breed) {
+//     super(name);
+//     this.breed = breed;
+//   }
+// }
+
+// class Cat extends Animal {
+//   constructor(name, breed) {
+//     super(name);
+//     this.breed = breed;
+//   }
+// }
+
+// let juniper = new Cat('Juniper', 'Rag Doll');
+
+// class Food {
+//   constructor(catagory) {
+//     this.catagory = catagory;
+//   }
+//   munch() {
+//     console.log(`${this.catagory} yummy food.`);
+//   }
+// }
+
+// class Meat extends Food {
+//   constructor(catagory, animal, cut) {
+//     super(catagory);
+//     this.animal = animal;
+//     this.cut = cut;
+//   }
+// }
+
+// let beef = new Meat('meat', 'cow', 'tri-tip');
