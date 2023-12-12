0699c5f1a74b0bc87f48da6a5ea01ced13513872
CoffeeNomad <28507502+rclardy@users.noreply.github.com>
2023-12-12 06:29:33 -0800

    finished part 1

--- classes/super-reviewJS/super-review/age-checker/js/main.js
+++ classes/super-reviewJS/super-review/age-checker/js/main.js
@@ -5,7 +5,22 @@
 //If under 25, tell them they can not rent cars affordably
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
-
+const age = 0;
+if (age < 16) {
+  console.log('You can not drive');
+} else if (age < 18) {
+  console.log(
+    "You can't hate from outside the club, because they can't even get in"
+  );
+} else if (age < 21) {
+  console.log('You can not drink!');
+} else if (age < 25) {
+  console.log('You can rent cars affordably');
+} else if (age < 30) {
+  console.log('You can not rent fancy cars affordably');
+} else if (age > 30) {
+  console.log('There is nothing left to look forward too...');
+}
 
 //--- Harder
 //On click of the h1
