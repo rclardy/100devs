6317663dfc1bf76ef461f8173fb5d1f8241a0160
CoffeeNomad <28507502+rclardy@users.noreply.github.com>
2023-12-13 08:13:07 -0800

    celcius to fahrenheight

--- classes/super-reviewJS/super-review/temperature-converter/js/main.js
+++ classes/super-reviewJS/super-review/temperature-converter/js/main.js
@@ -1,1 +1,14 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+//c to f
+
+document.querySelector('h1').addEventListener('click', cToF);
+
+function cToF() {
+  //need the value in c
+  let temp = Number(document.querySelector('input').value);
+
+  //convert c to f
+  temp = temp * 1.8 + 32;
+  //show the value
+  document.querySelector('h2').innerText = temp;
+}
