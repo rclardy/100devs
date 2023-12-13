28fa3e0dcc9b7403233e1606ce834b25a281249b
CoffeeNomad <28507502+rclardy@users.noreply.github.com>
2023-12-13 13:46:00 -0800

    ComareArrays

--- classes/super-reviewJS/super-review/review-in-paradise-after-paradise/js/main.js
+++ classes/super-reviewJS/super-review/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,15 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+function comareArray(arr) {
+  if (arr[0] < arr[arr.length - 1]) {
+    alert('Hi');
+  } else if (arr[0] > arr[arr.length - 1]) {
+    alert('Bye');
+  } else {
+    alert('We close in an hour');
+  }
+}
+
+comareArray([1, 2, 3, 4, 5]);
+comareArray([5, 2, 3, 4, 1]);
+comareArray([5, 2, 3, 4, 5]);
