30c516f2dda94e9701081377df077b4a7a0d1b02
CoffeeNomad <28507502+rclardy@users.noreply.github.com>
2023-12-14 13:37:17 -0800

    object litteral notation

--- classes/super-reviewJS/super-review/objects-listen-to-your-heart/js/main.js
+++ classes/super-reviewJS/super-review/objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,17 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {};
+
+stopwatch.color = 'black';
+stopwatch.shape = 'round';
+stopwatch.brand = 'Nike';
+stopwatch.smell = 'new';
+
+stopwatch.start = function () {
+  console.log('Runnnnnnn');
+};
+stopwatch.stop = function () {
+  console.log('Stop RUNNING');
+};
+stopwatch.morph = function () {
+  console.log('ITS MORPHINN TIMMEEEE');
+};
