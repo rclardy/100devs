46a3e7a0fff7e74698549ad0c9e5eb2d53259665
CoffeeNomad <28507502+rclardy@users.noreply.github.com>
2023-12-14 20:09:21 -0800

    creating objects with constructor

--- classes/super-reviewJS/super-review/objects-street-fighter/js/main.js
+++ classes/super-reviewJS/super-review/objects-street-fighter/js/main.js
@@ -1,1 +1,19 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function StreeFighter(name, health, speed, specialMove) {
+  this.name = name;
+  this.health = health;
+  this.speed = speed;
+  this.specialMove = specialMove;
+
+  this.throw = function () {
+    console.log('YEEETTT');
+  };
+  this.taunt = function () {
+    console.log(`You can't handle my ${this.specialMove}!!`);
+  };
+  this.fatality = function () {
+    console.log('GETTT OVVEEERRR HEERRRREEEE');
+  };
+}
+
+let chunLi = new StreeFighter('Chun Li', 100, 100, 'Spinning Bird Kick');
