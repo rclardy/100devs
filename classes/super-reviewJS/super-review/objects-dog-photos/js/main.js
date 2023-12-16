fa4368ab3d13a1e68e65de0e6c6aeb968c63ce0f
CoffeeNomad <28507502+rclardy@users.noreply.github.com>
2023-12-16 08:57:11 -0800

    messing around with more APIs

--- classes/super-reviewJS/super-review/objects-dog-photos/js/main.js
+++ classes/super-reviewJS/super-review/objects-dog-photos/js/main.js
@@ -1,10 +1,23 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
-fetch('https://dog.ceo/api/breeds/image/random')
+const url =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+
+fetch(url)
   .then((res) => res.json()) //parse response as JSON
   .then((data) => {
-    console.log(data.message);
-    document.querySelector('img').src = data.message;
+    console.log(data.drinks[0].strDrinkThumb);
+    document.querySelector('img').src = data.drinks[0].strDrinkThumb;
   })
   .catch((err) => {
     console.log(`error ${err}`);
   });
+
+// fetch('https://dog.ceo/api/breeds/image/random')
+//   .then((res) => res.json()) //parse response as JSON
+//   .then((data) => {
+//     console.log(data.message);
+//     document.querySelector('img').src = data.message;
+//   })
+//   .catch((err) => {
+//     console.log(`error ${err}`);
+//   });
